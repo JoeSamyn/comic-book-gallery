1134faf1f0c40b74168228d3d288b0b9b59b8480
Joe Samyn <19joe.samyn96@gmail.com>
2019-02-04 22:11:18 -0700

    Added redirect to Detail

--- Controllers/ComicBookController.cs
+++ Controllers/ComicBookController.cs
@@ -4,9 +4,25 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBookController : Controller
     {
-        public string Detail()
+
+        public string Index() 
         {
-            return "Hello from the comic books controller";
+            return "This is the homepage";
+        }
+
+        // The return type is action result because RedirectResult and ContentResult
+        // both have a base class of ActionResult.  This way we can return either. 
+        // Because of this ActionResult is almost always used as return type. 
+        public ActionResult Detail()
+        {
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Friday)
+            {
+                // Can user Redirect insted of new RedirectResult()
+                return Redirect("/");
+            }
+
+            // Can user Content insted of new ContentResult()
+            return Content("Here is a gallery about comic books!");
         }
     }
 }
