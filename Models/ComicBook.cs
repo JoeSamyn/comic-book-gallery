b4f368da831b53565d020068764fd3376e738d64
Joe Samyn <19joe.samyn96@gmail.com>
2019-02-05 09:50:19 -0700

    Added ComicBook and Artist models

---
+++ Models/ComicBook.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace ComicBookGallery.Models
+{
+    public class ComicBook
+    {
+        public int Id { get; set; }
+        public string SeriesTitle { get; set; }
+        public int IssueNumber { get; set; }
+        public string DescriptionHtml { get; set; }
+        public string[] Artists { get; set; }
+        public bool Favorite { get; set; }
+    }
+}
+
