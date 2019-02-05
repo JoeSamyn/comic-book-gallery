1f922bdcc65ba342df46461aa1d98bbc1743bb1d
Joe Samyn <19joe.samyn96@gmail.com>
2019-02-05 09:07:41 -0700

    Updated ComicBook Detail View

--- Controllers/ComicBookController.cs
+++ Controllers/ComicBookController.cs
@@ -22,6 +22,19 @@ namespace ComicBookGallery.Controllers
             return Content("Here is a gallery about comic books!");
             */
 
+            ViewBag.SeriesTitle = "The Amazing Spiderman";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
+            ViewBag.Artists = new string[]
+            {
+
+                "Script: Dan Slott",
+                "Pencils: Humberto Ramos",
+                "Inks: Victor Olazaba",
+                "Colors: Edgar Delgado",
+                "Letters: Chris Eliopoulos"
+            };
+
             return View();    
         }
     }
