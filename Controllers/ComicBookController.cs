c8aba6ace08deef0ed4302d0a2b8b92ed1b11d63
Joe Samyn <19joe.samyn96@gmail.com>
2019-02-06 15:26:03 -0700

    Updated ComicBookRepository and added Id properties to ComicBook

--- Controllers/ComicBookController.cs
+++ Controllers/ComicBookController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Web.Mvc;
+using ComicBookGallery.Data;
 using ComicBookGallery.Models;
 
 namespace ComicBookGallery.Controllers
@@ -10,36 +11,22 @@ namespace ComicBookGallery.Controllers
         // The return type is action result because RedirectResult and ContentResult
         // both have a base class of ActionResult.  This way we can return either. 
         // Because of this ActionResult is almost always used as return type. 
-        public ActionResult Detail()
+
+        private ComicBookRepository _comicBookRepository = null;
+
+        public ComicBookController()
         {
-            /*
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Friday)
+            _comicBookRepository = new ComicBookRepository();
+
+        }
+
+        public ActionResult Detail(int? id)
+        {
+            if (id == null)
             {
-                // Can user Redirect insted of new RedirectResult()
-                return Redirect("/");
+                return HttpNotFound();
             }
-
-            // Can user Content insted of new ContentResult()
-            // Checking version control
-            return Content("Here is a gallery about comic books!");
-            */
-
-            var comicBook = new ComicBook()
-            {
-                SeriesTitle = "The Amazing Spiderman",
-                IssueNumber = 700,
-                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, " +
-                	"great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
-                Artists = new Artist[]
-                {
-                    new Artist() {Name = "Dan Slott", Role = "Script:"},
-                    new Artist() {Name = "Humberto Ramos", Role = "Pencils:"},
-                    new Artist() {Name = "Victor Olazaba", Role = "Inks:"},
-                    new Artist() {Name = "Edgar Delgado", Role = "Colors:"},
-                    new Artist() {Name = "Chris Eliopoulos", Role = "Letters:"},
-                }
-            };
-
+            var comicBook = _comicBookRepository.GetComicBook((int)id);
             return View(comicBook);    
         }
 
