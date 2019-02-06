cf82ac151f5f47c1580b94b4ea6eba827f880cc3
Joe Samyn <19joe.samyn96@gmail.com>
2019-02-06 12:05:59 -0700

    Updated ComicBook Detail View Layout

--- Models/ComicBook.cs
+++ Models/ComicBook.cs
@@ -23,6 +23,7 @@ namespace ComicBookGallery.Models
             get
             {
                 return SeriesTitle.Replace(" ", "-").ToLower() + "-" + IssueNumber + ".jpg";
+
             }
         }
     }
